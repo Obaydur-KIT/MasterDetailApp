c81bf4810586d3f49032f48f6a31baaa5a852a0f
Obaydur - KIT <Obaydur - KIT@DESKTOP-JRN4S7D>
2024-06-10 16:47:55 +0600

    Add project files.

---
+++ MasterDetailApp/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MasterDetailApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <h3 align="center">Student Info</h3>
+
+        </div>
+
+            <asp:Table ID="Table1" runat="server">
+                <asp:TableRow>
+                    <asp:TableCell>Student ID : </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Student Name : </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Student Age : </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Contact : </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                
+            </asp:Table>
+        <div>
+            <br />
+           
+           
+            <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="Search" OnClick="Button4_Click" />
+            
+            <asp:GridView ID="GridView1" runat="server" Align ="center"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MasterDetailApp/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasterDetailApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-JRN4S7D;Initial Catalog=MatserDetaildb;Integrated Security=True;Encrypt=False;Application Name=EntityFramework");
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("Insert into Student_Info values('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+ int.Parse(TextBox3.Text) + "','"+ int.Parse(TextBox4.Text) + "')", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this,this.GetType(), "script", "alert('Succesfully Insert');",true);
+            LoadRecord();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("update Student_Info set Student_Name = '" + TextBox2.Text + "', Student_Age = '" + int.Parse(TextBox3.Text) + "', Contact = '" + TextBox4.Text + "' where Student_ID = '" + int.Parse(TextBox1.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Succesfully Updated');", true);
+            LoadRecord();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("delete Student_Info where Student_ID = '" + int.Parse(TextBox1.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Succesfully Deleted');", true);
+            LoadRecord();
+        }
+       
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("select * from Student_Info where Student_ID = '" + int.Parse(TextBox1.Text) + "'", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("select * from Student_Info ", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
